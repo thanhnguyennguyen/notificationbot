917041f844c7181f747fcb9b1a02ab987abed2a6
NguyenNguyen <nguyenbk92@gmail.com>
2020-04-28 14:49:22 +0700

    check if token, target empty

--- index.js
+++ index.js
@@ -2,20 +2,23 @@ const {
     exec
 } = require('child_process')
 const notifyTelegram = (msg, token, target) => {
-    let cmd = "curl -X POST -H 'Content-Type: application/json' -d '{\"chat_id\": \"" + target + "\", \"text\": \"" + msg + "\"}' https://api.telegram.org/bot" + token + "/sendMessage"
-    console.log(cmd)
-    exec(cmd)
+    if (token != '' && target != '') {
+        let cmd = "curl -X POST -H 'Content-Type: application/json' -d '{\"chat_id\": \"" + target + "\", \"text\": \"" + msg + "\"}' https://api.telegram.org/bot" + token + "/sendMessage"
+        console.log(cmd)
+        exec(cmd)
+    }
 }
 const notifySlack = (msg, token, target, botname, boticon) => {
-    botname = botname || "noti-bot"
-    boticon = boticon || "slack"
-    let cmd = "curl -X POST --data-urlencode \"payload={\\\"channel\\\": \\\"" + target + "\\\", \\\"username\\\": \\\"" + botname + "\\\", \\\"text\\\": \\\"" + msg + "\\\", \\\"icon_emoji\\\": \\\":" + boticon + ":\\\"}\" https://hooks.slack.com/services/" + token
-    console.log(cmd)
-    exec(cmd)
+    if (token != '' && target != '') {
+        botname = botname || "noti-bot"
+        boticon = boticon || "slack"
+        let cmd = "curl -X POST --data-urlencode \"payload={\\\"channel\\\": \\\"" + target + "\\\", \\\"username\\\": \\\"" + botname + "\\\", \\\"text\\\": \\\"" + msg + "\\\", \\\"icon_emoji\\\": \\\":" + boticon + ":\\\"}\" https://hooks.slack.com/services/" + token
+        console.log(cmd)
+        exec(cmd)
+    }
 }
 
 module.exports = {
     slack: notifySlack,
     telegram: notifyTelegram,
 }
-
