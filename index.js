53355915e81e7e95f71dd1ae8fca353b039e414b
NguyenNguyen <nguyenbk92@gmail.com>
2020-10-24 15:57:22 +0700

    v1.0.3: trim token, target

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const {
     exec
 } = require('child_process')
 const notifyTelegram = (msg, token, target) => {
-    if (token != '' && target != '') {
+    if (token.trim() != '' && target.trim() != '') {
         let cmd = "curl -X POST -H 'Content-Type: application/json' -d '{\"chat_id\": \"" + target + "\", \"text\": \"" + msg + "\"}' https://api.telegram.org/bot" + token + "/sendMessage"
         exec(cmd)
     }
 }
 const notifySlack = (msg, token, target, botname, boticon) => {
-    if (token != '' && target != '') {
+    if (token.trim() != '' && target.trim() != '') {
         botname = botname || "noti-bot"
         boticon = boticon || "slack"
         let cmd = "curl -X POST --data-urlencode \"payload={\\\"channel\\\": \\\"" + target + "\\\", \\\"username\\\": \\\"" + botname + "\\\", \\\"text\\\": \\\"" + msg + "\\\", \\\"icon_emoji\\\": \\\":" + boticon + ":\\\"}\" https://hooks.slack.com/services/" + token
