8c5a00bdd6421ee87955b5a04a4499e5b8d74e5c
NguyenNguyen <nguyenbk92@gmail.com>
2021-07-23 17:54:23 +0700

    fix notifyDiscord

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const notifySlack = (msg, token, target, botname, boticon) => {
 }
 
 const notifyDiscord = (msg, webhook, target) => {
-    if (webhook.trim() != '' && target.trim() != '') {
+    if (webhook.trim() != '') {
         let cmd = "curl -X POST --data '{\"content\":\"" + msg + "\"}' --header \"Content-Type:application/json\" " + webhook
         exec(cmd)
     }
