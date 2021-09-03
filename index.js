c7020ac9be320b5693982c38e8467d95cbeef3cd
NguyenNguyen <nguyenbk92@gmail.com>
2021-09-03 21:59:38 +0700

    Remove slash in messages

--- index.js
+++ index.js
@@ -1,30 +1,76 @@
-const {
-    exec
-} = require('child_process')
-const notifyTelegram = (msg, token, target, isHtmlMode) => {
+const axios = require('axios').default
+
+
+const notifyTelegram = async (msg, token, target, isHtmlMode) => {
     if (token.trim() != '' && target.trim() != '') {
-        let cmd
-        if (isHtmlMode) {
-            cmd = `curl -X POST -H 'Content-Type: application/json' -d '{"chat_id": "${target}", "text": "${msg}", "parse_mode": "html", "disable_web_page_preview": true}' https://api.telegram.org/bot${token}/sendMessage`
-        } else {
-            cmd = `curl -X POST -H 'Content-Type: application/json' -d '{"chat_id": "${target}", "text": "${msg}"}' https://api.telegram.org/bot${token}/sendMessage`
+        let params = {
+            "chat_id" : target,
+            "text": msg,
         }
-        exec(cmd)
+        if (isHtmlMode) {
+            params.parse_mode = 'html'
+            params.disable_web_page_preview = true
+        }
+        const data = Object.keys(params)
+          .map((key, index) => `${key}=${encodeURIComponent(params[key])}`)
+          .join('&');
+        
+        const options = {
+            method: 'POST',
+            headers: { 'content-type': 'application/x-www-form-urlencoded' },
+            data,
+            url: `https://api.telegram.org/bot${token}/sendMessage`,
+          };
+          await axios(options)
     }
 }
-const notifySlack = (msg, token, target, botname, boticon) => {
+const notifySlack = async (msg, token, target, botname, boticon) => {
     if (token.trim() != '' && target.trim() != '') {
         botname = botname || "noti-bot"
         boticon = boticon || "slack"
-        let cmd = `curl -X POST --data-urlencode "payload={\"channel\": \"${target}\", \"username\": \"${botname}\", \"text\": \"${msg}\", \"icon_emoji\": \":${boticon}:\"}" https://hooks.slack.com/services/${token}`
-        exec(cmd)
+        
+        let params = {
+            payload: JSON.stringify({
+                "channel" : target,
+                "username":  botname,
+                "text": msg,
+                "icon_emoji": boticon,
+            
+            })
+        }
+        const data = Object.keys(params)
+          .map((key, index) => `${key}=${encodeURIComponent(params[key])}`)
+          .join('&');
+        
+        const options = {
+            method: 'POST',
+            headers: { 'content-type': 'application/x-www-form-urlencoded' },
+            data,
+            url: `https://hooks.slack.com/services/${token}`,
+          };
+
+          await axios(options)
     }
 }
 
-const notifyDiscord = (msg, webhook, target) => {
+const notifyDiscord = async (msg, webhook) => {
     if (webhook.trim() != '') {
-        let cmd = `curl -X POST --data '{"content":"${msg}"}' --header "Content-Type:application/json" ${webhook}`
-        exec(cmd)
+        let params = {
+            "content": msg,
+        }
+
+        const data = Object.keys(params)
+          .map((key, index) => `${key}=${encodeURIComponent(params[key])}`)
+          .join('&');
+        
+        const options = {
+            method: 'POST',
+            headers: { 'content-type': 'application/x-www-form-urlencoded' },
+            data,
+            url: `${webhook}`,
+          };
+
+          await axios(options)
     }
 }
 
